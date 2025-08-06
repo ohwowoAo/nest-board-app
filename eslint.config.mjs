d6fda3288dfadabd050969dd741a71c43a3416dd
EunJeong <sbjdnm@gmail.com>
2025-08-06 11:29:32 +0900

    fix: ESLint 및 Prettier 경고 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+//문법에러체크
 // @ts-check
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
@@ -28,7 +29,9 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'prettier/prettier': ["warn", { "endOfLine": "auto" }]
+
     },
   },
 );
