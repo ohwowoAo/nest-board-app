80c95e29e52bfe8fd695077ecccb6d3340fccda2
EunJeong <sbjdnm@gmail.com>
2025-08-07 11:45:11 +0900

    feat: 게시판 Get/Post API 구현

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  //private 사용하지않으면 다른 컴포넌트에서 borards 배열에 접근할 수 있다.
+  private boards: Board[] = [];
+
+  getAllBoards(): Board[] {
+    return this.boards;
+  }
+
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+    this.boards.push(board);
+    return board;
+  }
+}
