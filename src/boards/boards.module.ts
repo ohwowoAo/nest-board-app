fc89e1d5622e760aa0bf70c00444d9202d938bf7
EunJeong <sbjdnm@gmail.com>
2025-08-12 15:58:12 +0900

    refactor: @EntityRepository 제거 및 @InjectRepository(Board)로 마이그레이션

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
-import { BoardRepository } from './board.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Board } from './board.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BoardRepository])],
+  imports: [TypeOrmModule.forFeature([Board])],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
