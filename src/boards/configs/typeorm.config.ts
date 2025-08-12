8ebfe5822fc4f34b5b867dc21621d07fa9496a06
EunJeong <sbjdnm@gmail.com>
2025-08-12 12:22:15 +0900

    feat : mySQL 연결

--- src/boards/configs/typeorm.config.ts
+++ src/boards/configs/typeorm.config.ts
@@ -4,11 +4,21 @@ import { ConfigService } from '@nestjs/config';
 
 export const typeORMConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
   type: 'mysql',
-  host: configService.get<string>('DB_HOST'),
-  port: configService.get<number>('DB_PORT'),
-  username: configService.get<string>('DB_USERNAME'),
-  password: configService.get<string>('DB_PASSWORD'),
-  database: configService.get<string>('DB_NAME'),
-  entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: true, // 개발용
+  host: configService.get<string>('DB_HOST', '127.0.0.1'),
+  port: Number(configService.get<string>('DB_PORT', '3306')),
+  username: configService.get<string>('DB_USER', 'root'),
+  password: configService.get<string>('DB_PASS', ''),
+  database: configService.get<string>('DB_NAME', 'nestjs_board'),
+  autoLoadEntities: true,
+  synchronize: true, // 개발만 true
+  logging: ['query','error','warn'],
+  charset: 'utf8mb4_general_ci',
+  // type: 'postgres',
+  // host: 'localhost',
+  // port: 5432,
+  // username: 'postgres',
+  // password: 'postgres',
+  // database: 'nestjs_board',
+  // entities: [__dirname + '/../**/*.entity.{js,ts}'],                        
+  // synchronize: true, // 개발용 
 });
