cca4b92f2c1a98c2ad449bde298899610f7cd119
EunJeong <sbjdnm@gmail.com>
2025-08-13 11:55:21 +0900

    refactor(auth): user.repository.ts 제거

--- src/auth/user.repository.ts
+++
@@ -1,8 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { User } from "./user.entity";
-
-@EntityRepository(User)
-export class UserRepository extends Repository<User> {
-  // UserRepository에 대한 메서드 정의
-  // 예: findById, createUser 등
-}   
