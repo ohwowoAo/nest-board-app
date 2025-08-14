eb16079f1e1e84e635c1c26a03da1dc315c06b5e
EunJeong <sbjdnm@gmail.com>
2025-08-14 14:57:09 +0900

    feat : 회원 비밀번호 암호화

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -13,7 +14,11 @@ export class AuthService {
 
     async signUp(dto: AuthCredentialDto): Promise<User> {
       const { username, password } = dto;
-      const user = this.userRepo.create({ username, password });
+
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      const user = this.userRepo.create({ username, password: hashedPassword });
 
       try {
             return await this.userRepo.save(user);
