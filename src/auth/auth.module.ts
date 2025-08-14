eb3bf949eca29472c0bae671b5f9aabd39271ece
EunJeong <sbjdnm@gmail.com>
2025-08-14 15:48:46 +0900

    feat : JWT 모듈 등록

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,15 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],  
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'defaultSecretKey',
+      signOptions: { expiresIn: '1h' }, // 토큰 만료 시간
+    }),
+    TypeOrmModule.forFeature([User])],  
   controllers: [AuthController],
   providers: [AuthService]
 })
