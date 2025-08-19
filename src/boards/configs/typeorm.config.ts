0c15ce2a344107c5e1548c2929cefcd70e1b6c5e
EunJeong <sbjdnm@gmail.com>
2025-08-19 17:27:38 +0900

    refactor: 서버 포트 환경변수 통일

--- src/boards/configs/typeorm.config.ts
+++ src/boards/configs/typeorm.config.ts
@@ -3,22 +3,14 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 
 export const typeORMConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
-  type: 'mysql',
-  host: configService.get<string>('DB_HOST', '127.0.0.1'),
-  port: Number(configService.get<string>('DB_PORT', '3306')),
-  username: configService.get<string>('DB_USER', 'root'),
-  password: configService.get<string>('DB_PASS', ''),
-  database: configService.get<string>('DB_NAME', 'nestjs_board'),
-  autoLoadEntities: true,
-  synchronize: true, // 개발만 true
-  logging: ['query','error','warn'],
+  type: configService.get<'mysql'>('DB_TYPE'),
+  host: configService.get<string>('DB_HOST'),
+  port: Number(configService.get<string>('DB_PORT')),
+  username: configService.get<string>('DB_USER'),
+  password: configService.get<string>('DB_PASS'),
+  database: configService.get<string>('DB_NAME'),
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: configService.get<string>('NODE_ENV') !== 'production', // 운영에서는 false
+  logging: ['query', 'error', 'warn'],
   charset: 'utf8mb4_general_ci',
-  // type: 'postgres',
-  // host: 'localhost',
-  // port: 5432,
-  // username: 'postgres',
-  // password: 'postgres',
-  // database: 'nestjs_board',
-  // entities: [__dirname + '/../**/*.entity.{js,ts}'],                        
-  // synchronize: true, // 개발용 
 });
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-  Logger.log(`Server running on http://localhost:${process.env.PORT ?? 3000}`, 'Bootstrap');
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('SERVER_PORT');
+
+  
+  if (!port) {
+    throw new Error('❌ SERVER_PORT is not defined in .env');
+  }
+  await app.listen(port);
+  Logger.log(
+    `🚀 Server running on http://localhost:${port}`,
+    'Bootstrap',
+  );
 }
 void bootstrap();
