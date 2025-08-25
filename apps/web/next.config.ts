d5c93ecd73e8b7d9bb5e7fe9490a087edd2584dc
EunJeong <sbjdnm@gmail.com>
2025-08-25 09:46:42 +0900

    feat: api 연동 react query 세팅

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,10 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/:path*",
+        source: '/api/:path*',
         destination: `http://localhost:${process.env.API_PORT ?? 4000}/:path*`,
       },
     ];
